551e3b8450a304363bf87a51fbb8ecb66eaf2928
Joy <joysu2008@gmail.com>
2026-01-25 09:55:21 -0500

    redo player as animatedsprite2d

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var sprite = $AnimatedSprite2D
 @onready var light = $PointLight2D
 @export var speed = 75
 
@@ -11,6 +12,15 @@ func _physics_process(_delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
 	move_and_slide()
+	# animations
+	if velocity.length() > 0:
+		sprite.play("moving")
+		if direction.x > 0:
+			sprite.flip_h = true
+		elif direction.x < 0:
+			sprite.flip_h = false
+	else:
+		sprite.play("static")
 
 func _ready():
 	light.scale = Vector2(0.4, 0.4)
